738d4f375f7faf57f89345cd77f87a7934af31ac
JuanPaLA <juanlopezalurralde@gmail.com>
2021-11-12 19:04:04 -0300

    revamp changes populating routes

--- routes/inputRoutes.js
+++ routes/inputRoutes.js
@@ -1,24 +1,48 @@
 var express = require('express');
 var inputRouter = express.Router();
-var inputModel = require('../database/mongo/inputModel');
-var User = require('../database/mongo/userModel');
-var {getinputsByUserAndVariables} = require('../controller/utilController');
+var Input = require('../database/mongo/Input');
+var User = require('../database/mongo/User');
 
 //GET INPUTS BY USERS ID and INPUTS NAMES
 var inputs = ['ingreso', 'gastos']
 var id = '6185c7cf68d6c5cf24be977e'
 
-//GET ALL INPUTS
-inputRouter.get('/inputs', async (req,res)=>{
-    res.send(await inputModel.find())
-})
+let joe, input;
 
-//GET ALL INPUTS BY USER AND VARIABLE NAMES
-inputRouter.get('/inputs/:id_user', async (req,res)=>{
-    res.json(await getinputsByUserAndVariables(req.params.id_user,inputs))
-})
+inputRouter.get('/', (req,res)=>{
+    joe = new User({username: 'JoeB'})
+    
+    input = new Input({name: 'ingreso', value: 12})
 
+    joe.inputs.push(input);
+    input.user = joe;
 
+    Promise.all([
+        joe.save(),
+        input.save()])
+    .then((results)=> 
+        res.json(results));
+});
+
+inputRouter.get('/populated', async (req,res)=>{
+    var results = await User.find({username: 'JoeB'})
+    .populate('userindicators')
+    res.json(results)
+});
+
+inputRouter.get('/fullpopulated', async (req,res)=>{
+    var results = await User.find({username: 'JoeB'})
+    .populate({
+        path: 'userindicators',
+        populate:{
+            path: 'inputs',
+            populate:{
+                path: 'user'
+            }
+        }
+    })
+    res.json(results)
+});
 
 module.exports = inputRouter;
 
